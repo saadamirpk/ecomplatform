21d1966321ad9625a48dfb0aae179360e2a18d35
Shahzaib Arshad <shahzaib99193150@gmail.com>
2019-12-31 00:42:14 +0500

    view -> template(folder) -> header,footer AND me
    ab template sa header footer use karo bilwajha code likhna ke zaroort ni ah

---
+++ application/views/templates/header.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No Direct access is allowed');
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+      <title>Ecomeplatform</title>
+        <link href="assests/bootstrap/bootstrap.css">
+    </head>
+    <body>
+    
